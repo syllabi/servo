e81135da984b163a76b6c8ee2d99c0451931313c
Saurav M. Hiremath <sauravhiremath@gmail.com>
2020-11-30 23:10:47 +0530

    feat: add create-course function

--- courses/index.js
+++ courses/index.js
@@ -1,15 +1,83 @@
 "use strict";
 
-exports.create = (request, response) => {
+const mongoClient = require("mongodb").MongoClient;
+const { v4 } = require("uuid");
+
+exports.create = async (request, response) => {
+  try {
+    const client = await mongoClient.connect(process.env.CONNECTION_STRING, {
+      useUnifiedTopology: true,
+    });
+
+    if (!client) {
+      return response
+        .status(500)
+        .json({ body: "Server Error. Connection to DB Failed!" });
+    }
+
+    const db = client.db("syllabi");
+
+    const {
+      courseId,
+      title,
+      thumbnail,
+      description,
+      university,
+    } = request.body;
+
+    if (!courseId || !title || !thumbnail || !university) {
+      return response.status(400).json({
+        body: "Missing Fields. Please try again",
+      });
+    }
+
+    const isExisting = await db
+      .collection("courses")
+      .findOne({ courseId, university }, { limit: 1 });
+
+    if (isExisting) {
+      return response.status(400).json({
+        body:
+          "Course for the required university already exists. Please try again",
+      });
+    }
+
+    const newCourse = {
+      id: v4(),
+      courseId,
+      title,
+      thumbnail,
+      description: description || "",
+      university,
+      playlists: [],
+      createdOn: new Date(),
+      lastModified: new Date(),
+    };
+
+    const doc = await db.collection("courses").insertOne(newCourse);
+
+    if (!doc) {
+      return response.status(400).json({
+        body: "Something went wrong. Please try again",
+      });
+    }
+
+    response.status(200).json({ body: doc });
+  } catch (err) {
+    response.status(500).json({ body: err.message });
+  }
+};
+
+exports.all = (request, response) => {
   const message = {
     path: request.path,
-    functionName: "create",
+    functionName: "all",
     subDir: "courses",
   };
   response.status(200).send(JSON.stringify(message));
 };
 
-exports.all = (request, response) => {
+exports.single = (request, response) => {
   const message = {
     path: request.path,
     functionName: "all",
